e8ac53fd5b0a728de535f4870e4bd7a2a8b3ff5a
Christopher Britt <christopher.brittjr@outlook.com>
2018-08-21 10:55:44 -0400

    Completed the addition to the storyline.

--- SpaceConsole/Intro.cs
+++ SpaceConsole/Intro.cs
@@ -13,24 +13,30 @@ namespace SpaceConsole
             Console.WriteLine("Greetings traveler, what shall we call you by? \n");
             string userName = Console.ReadLine();
             Console.WriteLine();
-            Console.WriteLine("You have entered {0}, are you sure?" + "Please type Yes or No.", userName);
+            Console.WriteLine("You have entered {0}, are you sure?  Please type Yes or No.", userName);
             string userAnswer = Console.ReadLine();
 
-            if (userAnswer == "No")
+            if (userAnswer.ToLower() == "no")
             {
+                Console.WriteLine("\nYou have answered No.\n");
                 StartPoint();
             }
-            else if (userAnswer == "Yes")
+            else if (userAnswer.ToLower() == "yes")
             {
-                //go to beginning of story line
+                Console.WriteLine("\nYou have answered Yes.\n");
+                BeginSL(userName);
             }
             else
             {
                 Console.WriteLine("You answer is invaild, please try again");
                 StartPoint();
-            }    
-
+            }
         }
-                    
+        public static void BeginSL(string userName)
+        {
+            Console.WriteLine("{0}, you have inherited the family ship in the trading business." , userName);
+            Console.WriteLine("Your family also left you with something else.....");
+            Console.WriteLine("The family debt of $1000000 credits.\n");
+        }
     }
 }
