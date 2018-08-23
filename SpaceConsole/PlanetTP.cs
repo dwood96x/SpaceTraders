7bb22e1f3ea4ac65fdde5a267ec4d2e117ec7e19
Christopher Britt <christopher.brittjr@outlook.com>
2018-08-23 11:38:30 -0400

    Editing in PlanetTP BuyItems and Sellitems

--- SpaceConsole/PlanetTP.cs
+++ SpaceConsole/PlanetTP.cs
@@ -120,16 +120,16 @@ namespace SpaceConsole
                 Console.WriteLine("Which item would you like to buy {0}\n", Inventory.userName);
                 Console.WriteLine("===========================================\n");
                 Console.WriteLine("\t1 Water\n\t2 Food\n\t3 Fuel" +
-                                 "\n\t4 Common Mineral\n\t5 Medical Supplies\n\tO6 Technology\n" +
-                                  "\tOption 7 Weapons\n\tOption 8 Rare Material\n\tOption 9 Dark Matter\n");
+                                 "\n\t4 Common Mineral\n\t5 Medical Supplies\n\t6 Technology\n" +
+                                  "\t7 Weapons\n\t8 Rare Material\n\t9 Dark Matter\n");
             }
         public static void SellItems()
         {
             Console.WriteLine("Which item would you like to sell {0}\n", Inventory.userName);
             Console.WriteLine("===========================================\n");
             Console.WriteLine("\t1 Water\n\t2 Food\n\t3 Fuel" +
-                                 "\n\t4 Common Mineral\n\t5 Medical Supplies\n\tO6 Technology\n" +
-                                  "\tOption 7 Weapons\n\tOption 8 Rare Material\n\tOption 9 Dark Matter\n");
+                                 "\n\t4 Common Mineral\n\t5 Medical Supplies\n\t6 Technology\n" +
+                                  "\t7 Weapons\n\t8 Rare Material\n\t9 Dark Matter\n");
         }
         public static void Leave()
         {
