63df8cf2ff9562d082e67a86adfe286553113e4e
Christopher Britt <christopher.brittjr@outlook.com>
2018-08-24 10:11:13 -0400

    Added coding to the BuyItems Class

--- SpaceConsole/BuyItems.cs
+++ SpaceConsole/BuyItems.cs
@@ -11,13 +11,53 @@ namespace SpaceConsole
         public static void BItems()
         {
                 Console.Clear();
-                Console.WriteLine("Which item would you like to buy {0}?  Please use the intergers only!\n", Inventory.userName);
+                Console.WriteLine("Which item would you like to buy {0}?\n", Inventory.userName);
                 Console.WriteLine("==================================");
                 Console.WriteLine("\n\t1 Water\n\t2 Food\n\t3 Fuel" +
                                   "\n\t4 Common Mineral\n\t5 Medical Supplies\n\t6 Technology" +
                                   "\n\t7 Weapons\n\t8 Rare Material\n\t9 Dark Matter\n" +
                                   "\n\t0 I do not want to buy anything!!!\n");
-                Console.WriteLine("==================================");          
+                Console.WriteLine("==================================");
+            string input = Console.ReadLine();
+
+            if (input.ToLower() == "water")
+            {
+
+            }
+            if (input.ToLower() == "water")
+            {
+
+            }
+            if (input.ToLower() == "water")
+            {
+
+            }
+            if (input.ToLower() == "water")
+            {
+
+            }
+            if (input.ToLower() == "water")
+            {
+
+            }
+            if (input.ToLower() == "water")
+            {
+
+            }
+            if (input.ToLower() == "water")
+            {
+
+            }
+            if (input.ToLower() == "water")
+            {
+
+            }
+            if (input.ToLower() == "water")
+            {
+
+            }
+
         }
+        
 }
 }
