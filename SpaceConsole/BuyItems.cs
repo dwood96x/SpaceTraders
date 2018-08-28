702f0820e99693ce08700eb923f1126eeea3f74b
Christopher Britt <christopher.brittjr@outlook.com>
2018-08-28 08:37:17 -0400

    Added coding to BuyItems.cs to allow user chose how much of an item they want.

--- SpaceConsole/BuyItems.cs
+++ SpaceConsole/BuyItems.cs
@@ -8,19 +8,76 @@ namespace SpaceConsole
 {
     class BuyItems
     {
-        public static string itemtoadd;
         public static void BItems()
         {
             Console.Clear();
             Console.WriteLine("Which item would you like to buy {0}?\n", Inventory.userName);
-            Console.WriteLine("==================================");
-            Console.WriteLine("\n\t1 Water - 1000c\n\t2 Food - 1200c\n\t3 Fuel - 2000c" +
-                              "\n\t4 Common Mineral - 5000c\n\t5 Medical Supplies - 10000c\n\t6 Technology - 15000c" +
-                              "\n\t7 Weapons - 20,000c\n\t8 Rare Material - 75,000c\n\t9 Dark Matter - 100,000c\n" +
-                              "\n\t0 I do not want to buy anything!!!\n");
-            Console.WriteLine("==================================");
+            Console.WriteLine("=========================================");
+            Console.WriteLine("\n\t Water -                     1000c" +
+                              "\n\t Food -                      1200c" +
+                              "\n\t Fuel -                      2000c" +
+                              "\n\t Common Mineral -            5000c" +
+                              "\n\t Medical Supplies -         10000c" +
+                              "\n\t Technology -               15000c" +
+                              "\n\t Weapons -                  20000c" +
+                              "\n\t Rare Material -            75000c" +
+                              "\n\t Dark Matter -             100000c" +
+                              "\n" +
+                              "\n\t Leave\n");
+            Console.WriteLine("=========================================");
             string input = Console.ReadLine();
-        }    
-        
+
+            switch (input)
+            {
+                case "water":
+                    Console.WriteLine("\nHow much water would you like to buy?\n");
+                    string water = Console.ReadLine();
+                    int a = Convert.ToInt32(water);
+                    break;
+                case "food":
+                    Console.WriteLine("\nHow much Food would you like to buy?\n");
+                    string food = Console.ReadLine();
+                    break;
+                case "fuel":
+                    Console.WriteLine("\nHow much Fuel would you like to buy?\n");
+                    string fuel = Console.ReadLine();
+                    break;
+                case "common mineral":
+                    Console.WriteLine("\nHow much Common Mineral would you like to buy?\n");
+                    string commonMineral = Console.ReadLine();
+                    break;
+                case "medical supplies":
+                    Console.WriteLine("\nHow much Medical Supplies would you like to buy?\n");
+                    string medicalSupplies = Console.ReadLine();
+                    break;
+                case "technology":
+                    Console.WriteLine("\nHow much Technology would you like to buy?\n");
+                    string technology = Console.ReadLine();
+                    break;
+                case "weapons":
+                    Console.WriteLine("\nHow many Weapons would you like to buy?\n");
+                    string weapons = Console.ReadLine();
+                    break;
+                case "rare material":
+                    Console.WriteLine("\nHow much Rare Material would you like to buy?\n");
+                    string rareMaterial = Console.ReadLine();
+                    break;
+                case "dark matter":
+                    Console.WriteLine("\nHow much Dark Matter would you like to buy?\n");
+                    string darkMatter = Console.ReadLine();
+                    break;
+                case "leave":
+                    //need to get back to the current planet's options
+                    break;
+                default:
+                    Console.WriteLine("\nYou have enter an invaild item, please check your itema and try again.\n");
+                    break;
+            }
+
+        }
+        public static void userInput(string input)
+        {
+            
+        }        
     }
 }
