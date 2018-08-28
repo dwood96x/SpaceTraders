3992f2938765b445e0ca8e4904e6f0a61fdae245
David Wood <dwood96x@gmail.com>
2018-08-28 09:27:25 -0400

    Added credits end check and balanced ships.

--- SpaceConsole/Ship.cs
+++ SpaceConsole/Ship.cs
@@ -12,34 +12,40 @@ namespace SpaceConsole
         public static double maxSpeed;
         public static double curShipSize;
         public static double shipCost;
+        public static double maxFuel;
 
         public static void Krillan()
         {
-             shipName = "Krillan";
-             maxSpeed = 3;
-             curShipSize = 1;
-             shipCost = 2000;
+            shipName = "Krillan";
+            maxSpeed = 3;
+            curShipSize = 3;
+            shipCost = 2000;
+            maxFuel = 10;
         }
         public static void Piccolo()
         {
-             shipName = "Piccolo";
-             maxSpeed = 5;
-             curShipSize = 2;
-             shipCost = 15000;
+            shipName = "Piccolo";
+            maxSpeed = 5;
+            curShipSize = 6;
+            shipCost = 15000;
+            maxFuel = 15;
         }
         public static void Vegito()
         {
-             shipName = "Vegito";
-             maxSpeed = 7;
-             curShipSize = 3;
-             shipCost = 50000;
+            shipName = "Vegito";
+            maxSpeed = 7;
+            curShipSize = 9;
+            shipCost = 50000;
+            maxFuel = 20;
         }
         public static void MajinBuu()
         {
-             shipName = "Majin Buu";
-             maxSpeed = 6;
-             curShipSize = 4;
-             shipCost = 100000;
+            shipName = "Majin Buu";
+            maxSpeed = 6;
+            curShipSize = 12;
+            shipCost = 100000;
+            maxFuel = 25;
         }
+
     }
 }
