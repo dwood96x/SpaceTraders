913b1f7d17e224c42f2aee0b7a73332f65d56250
Christopher Britt <christopher.brittjr@outlook.com>
2018-08-29 11:29:23 -0400

    Added new class of Planet.  Code gives x, y location.

---
+++ SpaceConsole/Planet.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SpaceConsole
+{
+    class Planet
+    {
+        //public double a;
+        //public double b;
+        //public double c;
+        //public void distance(Planet currentPlant, Planet destination)
+        //{
+        //    a = currentPlant.xCoord - destination.xCoord;
+        //    b = currentPlant.yCoord - destination.xCoord;
+        //    c = Math.Sqrt(Math.Pow(a, 2) + Math.Pow(b, 2.0));
+        //}
+
+        //{
+        //    Planet earth = new Planet(0, 0, "Earth");
+        //Planet ac = new Planet(0, -4.367, "Alpha Centurion");
+        //Planet m63 = new Planet(-4.6, 5, "M63");
+        //Planet p100 = new Planet(-30, -30, "P100");
+        //Planet grenonus = new Planet(-36, -23, "Grenonus");
+        //Planet derioa = new Planet(-13, 12, "Derioa");
+        //Planet esides = new Planet(6, 35, "Esides");
+        //Planet geitov = new Planet(-28, 38, "Geirtov");
+        //Planet currentLocation = earth;
+
+        //Travel travel = new Travel();
+        //travel.distance(currentLocation, ac);
+        //}
+        
+
+        //This is the formula and coords for the 8 planets.
+        //Need to pass to program and intergrate to current code.
+}
+}
